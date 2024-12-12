730f505cde4a612356e15f52210480bc8d12bc0c
Lucas de Lima <lcslimas@gmail.com>
2024-12-12 00:50:55 -0300

    Feat: implements doubleLinkedList

--- src/day1/DoublyLinkedList.ts
+++ src/day1/DoublyLinkedList.ts
@@ -7,7 +7,7 @@ type Node<T> = {
 export default class DoublyLinkedList<T> {
     public length: number;
     private head?: Node<T>;
-    private tail?: Node<T>
+    private tail?: Node<T>;
 
     constructor() {
         this.length = 0;
@@ -15,109 +15,118 @@ export default class DoublyLinkedList<T> {
     }
 
     prepend(item: T): void {
-        const newNode = {value: item}
-        const headNode = this.head
+        const newNode = { value: item };
+        const headNode = this.head;
         this.length++;
-        if(!headNode) {
-            this.head = this.tail = newNode
-            return
+        if (!headNode) {
+            this.head = this.tail = newNode;
+            return;
         }
-        this.head = { ...newNode, next: headNode }
-        headNode.prev = this.head
+        this.head = { ...newNode, next: headNode };
+        headNode.prev = this.head;
     }
     insertAt(item: T, idx: number): void {
-        if(idx > this.length) {
+        if (idx > this.length) {
             throw new Error();
         }
         this.length++;
-        if(idx === this.length) {
+        if (idx === this.length) {
             this.append(item);
-        } else if(idx === 0) {
-            this.prepend(item)
+        } else if (idx === 0) {
+            this.prepend(item);
         }
-        const newNode = {value: item}
+        const newNode = { value: item };
         let actualNode = this.head;
         let count = 0;
-        while(actualNode && count < idx) {
-            actualNode = actualNode.next
+        while (actualNode && count < idx) {
+            actualNode = actualNode.next;
             count++;
         }
-        const previousNext = actualNode?.next
-        if(previousNext) {
-            previousNext.next = actualNode
+        const previousNext = actualNode?.next;
+        if (previousNext) {
+            previousNext.next = actualNode;
         }
-        if(actualNode)
-            actualNode.next = {...newNode, next: previousNext, prev: actualNode}
+        if (actualNode)
+            actualNode.next = {
+                ...newNode,
+                next: previousNext,
+                prev: actualNode,
+            };
     }
     append(item: T): void {
-        const newNode = {value: item}
-        if(!this.length) {
-            this.head = this.tail = newNode
+        const newNode = { value: item };
+        if (!this.length) {
+            this.head = this.tail = newNode;
+            this.length++;
+            return;
         }
         this.length++;
         const previousTail = this.tail;
-        this.tail = {...newNode, prev: previousTail}
-        if(previousTail) {
-            previousTail.next = this.tail
+        this.tail = { ...newNode, prev: previousTail };
+        if (previousTail) {
+            previousTail.next = this.tail;
         }
     }
     remove(item: T): T | undefined {
-        let actualItem = this.head
-        while(actualItem) {
-            if(actualItem === item) {
+        let actualItem = this.head;
+        while (actualItem) {
+            if (actualItem.value === item) {
                 const prevNode = actualItem.prev;
-                if(prevNode) {
-                    prevNode.next = actualItem.next
+                const next = actualItem.next;
+                if (prevNode) {
+                    prevNode.next = next;
+                    if (next) next.prev = prevNode;
                 } else {
-                    this.head = undefined
+                    this.head = next;
                 }
-                this.length = Math.max(0, this.length--);
+
+                this.length = this.length - 1 > 0 ? this.length - 1 : 0;
                 return actualItem.value;
             }
-            actualItem = actualItem.next
+            actualItem = actualItem.next;
         }
-        return undefined
+        return undefined;
     }
-    get(idx: number): T | undefined { 
-        let actualNode = this.head
-        for(let i = 0; actualNode && i <= idx; i++) {
-            actualNode = actualNode.next
+    get(idx: number): T | undefined {
+        let actualNode = this.head;
+        for (let i = 0; actualNode && i < idx; i++) {
+            actualNode = actualNode.next;
         }
         return actualNode?.value;
     }
     removeAt(idx: number): T | undefined {
-        let curr = this.head
-        let iterator = 0
-        if(idx === 0) {
+        let curr = this.head;
+        let iterator = 0;
+        if (idx === 0) {
             const headNode = this.head;
             const nextNode = headNode?.next;
-            if(nextNode) nextNode.prev = this.head
-            this.head = headNode?.next
-            this.length = Math.max(0, this.length--);
-            return headNode?.value
+            this.head = headNode?.next;
+            if (nextNode) nextNode.prev = undefined;
+            this.length = this.length - 1 > 0 ? this.length - 1 : 0;
+            return headNode?.value;
         }
         do {
-            curr = curr?.next
+            curr = curr?.next;
             iterator++;
-        } while (curr && iterator <= idx) 
+        } while (curr && iterator < idx);
 
-        if(curr) {
-            this.length--// = Math.max(0, this.length--);
+        if (curr) {
+            this.length--;
         }
 
         const previousCurr = curr?.prev;
-        const nextCur = curr?.next
+        const nextCur = curr?.next;
 
-        if(previousCurr) {
-            previousCurr.next = nextCur
+        if (previousCurr) {
+            previousCurr.next = nextCur;
         } else {
-            this.head = nextCur
+            this.head = nextCur;
         }
 
-        if(nextCur) {
+        if (nextCur) {
             nextCur.prev = previousCurr;
         }
 
-        return curr?.value
+        return curr?.value;
     }
 }
